658b8c15fc5ee14e7a41cbf817e22c5cb1e1fd50
dominicchildrv <114876361+dominicchildrv@users.noreply.github.com>
2022-10-07 14:34:47 +0100

    Solution finished

--- primes.py
+++ primes.py
@@ -3,4 +3,24 @@
 
 def primes(number_of_primes):
     list = []
+    count = 1
+    currentNumber = 2
+    while count < (number_of_primes) + 1:
+        isPrime = True
+        for i in range (2,currentNumber + 1):
+            if i == currentNumber:
+                pass
+            else:
+                if currentNumber%i == 0:
+                    isPrime = False
+        if isPrime == True:
+            count = count + 1
+            list.append(currentNumber)
+            currentNumber = currentNumber + 1
+
+        else:
+            currentNumber = currentNumber + 1
+
+
     return list
+
